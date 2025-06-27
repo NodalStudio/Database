5eab150d9e4a8ce3a1ef906e748e9c2a891a8248
olozano-p <ol347@nyu.edu>
2025-06-27 16:15:13 +0000

    ✨ Add ICTUS database and user creation to init-databases.sql

--- mysql-init/init-databases.sql
+++ mysql-init/init-databases.sql
@@ -6,6 +6,11 @@ CREATE DATABASE IF NOT EXISTS alumbra_db CHARACTER SET utf8mb4 COLLATE utf8mb4_u
 CREATE USER IF NOT EXISTS 'alumbra_user'@'%' IDENTIFIED BY 'alumbra_password';
 GRANT ALL PRIVILEGES ON alumbra_db.* TO 'alumbra_user'@'%';
 
+-- Create ICTUS database and user
+CREATE DATABASE IF NOT EXISTS ictus_db CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+CREATE USER IF NOT EXISTS 'ictus_user'@'%' IDENTIFIED BY 'ictus_password';
+GRANT ALL PRIVILEGES ON ictus_db.* TO 'ictus_user'@'%';
+
 -- Create a read-only monitoring user (optional)
 CREATE USER IF NOT EXISTS 'db_monitor'@'%' IDENTIFIED BY 'monitor_readonly_2024';
 GRANT SELECT ON *.* TO 'db_monitor'@'%';
